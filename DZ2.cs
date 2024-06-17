b9d1948eddb21586ccfd955c5b2f94412dd99e85
Kolokolova_Vik <2645436@gmail.com>
2024-06-17 11:05:22 +0300

    Homework1.3
    WorkingWithString

--- DZ2.cs
+++ DZ2.cs
@@ -1,19 +1,19 @@
-﻿namespace dz1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int numberCrystals;
-            float health;
-            bool isOpen;
-            string name;
-            char symbol;
-            long timeInSeconds;
-            int armor;
-            int money;
-            int damage;
-            bool isWork;
-        }
-    }
-}
+﻿//namespace dz1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int numberCrystals;
+//            float health;
+//            bool isOpen;
+//            string name;
+//            char symbol;
+//            long timeInSeconds;
+//            int armor;
+//            int money;
+//            int damage;
+//            bool isWork;
+//        }
+//    }
+//}
