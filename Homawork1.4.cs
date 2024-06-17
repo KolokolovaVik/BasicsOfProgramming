6f8ba3a55a4c11a829c4705cb7c1d8b42a43d0f1
Kolokolova_Vik <2645436@gmail.com>
2024-06-17 12:31:54 +0300

    Commit 17.06.24
    Homework 1.4

---
+++ Homawork1.4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Homawork1
+    {
+        static void Main(string[] args)
+        {
+            int filledRows;
+            int additionalImages;
+
+            int image = 52;
+            int inRowOfPictures = 3;
+
+            filledRows = image / inRowOfPictures;
+            additionalImages = image % inRowOfPictures;
+
+            Console.WriteLine($"Количество заполненных рядов картинками: {filledRows}, осталось картинок: {additionalImages}");
+        }  
+    }
+}
--- Homework1.3.cs
+++ Homework1.3.cs
@@ -1,26 +1,26 @@
-﻿using System;
+﻿//using System;
 
-namespace ConsoleApp2
-{
-    internal class Homework1
-    {
-        static void Main(string[] args)
-        {
-            string name;
-            int age;
-            string zodiacSign;
-            string job;
+//namespace ConsoleApp2
+//{
+//    internal class Homework1
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name;
+//            int age;
+//            string zodiacSign;
+//            string job;
 
-            Console.WriteLine("Приветствую! Как вас зовут?");
-            name = Console.ReadLine();
-            Console.WriteLine("Сколько вам лет?");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Кто вы по гороскопу?");
-            zodiacSign = Console.ReadLine();
-            Console.WriteLine("Кем мы работаете?");
-            job = Console.ReadLine();
+//            Console.WriteLine("Приветствую! Как вас зовут?");
+//            name = Console.ReadLine();
+//            Console.WriteLine("Сколько вам лет?");
+//            age = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Кто вы по гороскопу?");
+//            zodiacSign = Console.ReadLine();
+//            Console.WriteLine("Кем мы работаете?");
+//            job = Console.ReadLine();
 
-            Console.WriteLine($"\nВас зовут {name}, вам {age}, ваш знак зодиака {zodiacSign}. Вы работаете {job}.");
-        }
-    }
-}
+//            Console.WriteLine($"\nВас зовут {name}, вам {age}, ваш знак зодиака {zodiacSign}. Вы работаете {job}.");
+//        }
+//    }
+//}
