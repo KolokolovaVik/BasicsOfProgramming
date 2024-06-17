f04b4ea811d62340586511b561a717993251ccb0
Kolokolova_Vik <2645436@gmail.com>
2024-06-17 10:47:08 +0300

    Initial commit

---
+++ DZ2.cs
@@ -0,0 +1,19 @@
+﻿namespace dz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCrystals;
+            float health;
+            bool isOpen;
+            string name;
+            char symbol;
+            long timeInSeconds;
+            int armor;
+            int money;
+            int damage;
+            bool isWork;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.SqlTypes;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp2
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int food;
+//            int money;
+//            int foodUnitMoney = 10;
+//            bool isAbleToPay;
+
+//            Console.WriteLine($"Добро пожаловать! Еда по {foodUnitMoney} монет");
+//            Console.WriteLine("Сколько у вас золота?");
+//            money = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Сколько еды вы хотите?");
+//            food = Convert.ToInt32(Console.ReadLine());
+
+//            isAbleToPay = money >= food * foodUnitMoney;
+//            food *= Convert.ToInt32(isAbleToPay);
+//            money -= food * foodUnitMoney;
+//            Console.WriteLine($"У вас {food} еды и {money} монет золота");
+//        }
+//    }
+//}
