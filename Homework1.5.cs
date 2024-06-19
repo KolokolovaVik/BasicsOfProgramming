651a517249b9feeb8c958c5598db3a3ba7b52527
Kolokolova_Vik <2645436@gmail.com>
2024-06-19 19:15:20 +0300

    Number 3
    temp. Don't use $

--- Homework1.5.cs
+++ Homework1.5.cs
@@ -6,8 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string correctName;
-            string newCupOfTea;
+            string temp;
 
             string name = "Колоколова";
             string surname = "Вика";
@@ -17,16 +16,16 @@ namespace ConsoleApp2
             Console.WriteLine($"Ваше имя: {name}, ваша фамилия: {surname}");
             Console.WriteLine($"В чашке чая находится: {cupOfTea}, а в чашку кофе налили: {cupOfCoffee}");
 
-            Console.WriteLine($"\nИсправим ошибку с именем и фамилией!");
-            correctName = surname;
+            Console.WriteLine("\nИсправим ошибку с именем и фамилией!");
+            temp = surname;
             surname = name;
-            name = correctName;
+            name = temp;
             Console.WriteLine($"Ваше имя: {name}, ваша фамилия {surname}");
 
-            Console.WriteLine($"\nИсправим ошибку с напитками!");
-            newCupOfTea = cupOfCoffee;
+            Console.WriteLine("\nИсправим ошибку с напитками!");
+            temp = cupOfCoffee;
             cupOfCoffee = cupOfTea;
-            cupOfTea = newCupOfTea;
+            cupOfTea = temp;
             Console.WriteLine($"В чашке для чая налит: {cupOfTea}, а в чашке для кофе: {cupOfCoffee}");
         }
     }
