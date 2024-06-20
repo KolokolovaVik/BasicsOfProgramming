c4b915e3c9a06d177f71452786e89ebe56b2d2cd
Kolokolova_Vik <2645436@gmail.com>
2024-06-20 14:00:07 +0300

    Homework1.6
    20.06.2024

--- Homework1.5.cs
+++ Homework1.5.cs
@@ -1,32 +1,32 @@
-﻿using System;
+﻿//using System;
 
-namespace ConsoleApp2
-{
-    internal class Homework1
-    {
-        static void Main(string[] args)
-        {
-            string temp;
+//namespace ConsoleApp2
+//{
+//    internal class Homework1
+//    {
+//        static void Main(string[] args)
+//        {
+//            string temp;
 
-            string name = "Колоколова";
-            string surname = "Вика";
-            string cupOfTea = "Кофе";
-            string cupOfCoffee = "Чай";
+//            string name = "Колоколова";
+//            string surname = "Вика";
+//            string cupOfTea = "Кофе";
+//            string cupOfCoffee = "Чай";
 
-            Console.WriteLine($"Ваше имя: {name}, ваша фамилия: {surname}");
-            Console.WriteLine($"В чашке чая находится: {cupOfTea}, а в чашку кофе налили: {cupOfCoffee}");
+//            Console.WriteLine($"Ваше имя: {name}, ваша фамилия: {surname}");
+//            Console.WriteLine($"В чашке чая находится: {cupOfTea}, а в чашку кофе налили: {cupOfCoffee}");
 
-            Console.WriteLine("\nИсправим ошибку с именем и фамилией!");
-            temp = surname;
-            surname = name;
-            name = temp;
-            Console.WriteLine($"Ваше имя: {name}, ваша фамилия {surname}");
+//            Console.WriteLine("\nИсправим ошибку с именем и фамилией!");
+//            temp = surname;
+//            surname = name;
+//            name = temp;
+//            Console.WriteLine($"Ваше имя: {name}, ваша фамилия {surname}");
 
-            Console.WriteLine("\nИсправим ошибку с напитками!");
-            temp = cupOfCoffee;
-            cupOfCoffee = cupOfTea;
-            cupOfTea = temp;
-            Console.WriteLine($"В чашке для чая налит: {cupOfTea}, а в чашке для кофе: {cupOfCoffee}");
-        }
-    }
-}
+//            Console.WriteLine("\nИсправим ошибку с напитками!");
+//            temp = cupOfCoffee;
+//            cupOfCoffee = cupOfTea;
+//            cupOfTea = temp;
+//            Console.WriteLine($"В чашке для чая налит: {cupOfTea}, а в чашке для кофе: {cupOfCoffee}");
+//        }
+//    }
+//}
---
+++ Homework1.6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Homework1
+    {
+        static void Main(string[] args)
+        {
+            int crystals;
+            int goldCoins;
+            
+            int pricePerCrystal = 10;
+
+            Console.WriteLine($"Добро пожаловать в магазин кристаллов! Один кристалл оценивается в {pricePerCrystal} золотых монет." +
+                $" Сколько у вас золотых монет? ");
+            goldCoins = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов вы хотите купить? ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+            goldCoins -= crystals * pricePerCrystal;
+            Console.WriteLine($"У вас {goldCoins} золотых монет и {crystals} кристаллов.");
+        }
+    }
+}
