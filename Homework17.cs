0a7d2f4595554aa034785c3376dcbf6b8d82bf11
Kolokolova_Vik <2645436@gmail.com>
2024-06-27 12:11:20 +0300

    Commit Homework 1.7
    27.06.2024

---
+++ Homework17.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Homework17
+    {
+        static void Main(string[] args)
+        {
+            int patients;
+            int waitTime;
+            int hours;
+            int minutes;
+
+            int timeSpentOnePatient = 10;
+            int oneHourMinutes = 60;
+
+            Console.WriteLine("Введите кол-во людей в очереди:");
+            patients = Convert.ToInt32(Console.ReadLine());
+
+            waitTime = patients * timeSpentOnePatient;
+            hours = waitTime / oneHourMinutes;
+            minutes = waitTime % oneHourMinutes;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
+        }
+    }
+}
